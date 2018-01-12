90a1d308683b215114bae5868061421c27738c73
遼太郎 金 <150568@jc-21.jp>
2018-01-12 16:58:47 +0900

    asda

--- SamplePage/SamplePage/BookPage.xaml.cs
+++ SamplePage/SamplePage/BookPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
@@ -8,98 +9,117 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+
 namespace SamplePage
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class BookPage : ContentPage
     {
+        int j = 1;
         public BookPage()
         {
             InitializeComponent();
+            var query = UserModel.countUser(j);
 
+            var query2 = UserModel.selectUser();
             ObservableCollection<Book> items = new ObservableCollection<Book>();
-            items.Add(new Book { Name = "John Doe", Value = 4.0, BlueBook = true, RedStar = true });
-            items.Add(new Book { Name = "Jane Doe", Value = 3.5, BlueBook = true, RedStar = false });
-            items.Add(new Book { Name = "Sammy Doe", Value = 2.5, BlueBook = false, RedStar = false });
-
-            for (var i = 0; i < items.Count; i++)
+            var List1 = new List<String>();
+            //*をリストにぶち込んで個数分addするのでもいいのでは
+            foreach(var user in query2)
             {
-                try
-                {
-
-
-                    if (items[i].Value <= 0.25)
-                    {
-                        items[i].ValueImage = "value_0.gif";
-                    }
-
-                    else if (items[i].Value <= 0.75)
-                    {
-                        items[i].ValueImage = "value_0.5.gif";
-                    }
-
-                    else if (items[i].Value <= 1.25)
-                    {
-                        items[i].ValueImage = "value_1.gif";
-                    }
-
-                    else if (items[i].Value <= 1.75)
-                    {
-                        items[i].ValueImage = "value_1.5.gif";
-                    }
-
-                    else if (items[i].Value <= 2.25)
-                    {
-                        items[i].ValueImage = "value_2.gif";
-                    }
-
-                    else if (items[i].Value <= 2.75)
-                    {
-                        items[i].ValueImage = "value_2.5.gif";
-                    }
-
-                    else if (items[i].Value <= 3.25)
-                    {
-                        items[i].ValueImage = "value_3.gif";
-                    }
-
-                    else if (items[i].Value <= 3.75)
-                    {
-                        items[i].ValueImage = "value_3.5.gif";
-                    }
-
-                    else if (items[i].Value <= 4.25)
-                    {
-                        items[i].ValueImage = "value_4.gif";
-                    }
-
-                    else if (items[i].Value <= 4.75)
-                    {
-                        items[i].ValueImage = "value_4.5.gif";
-                    }
-
-                    else
-                    {
-                        items[i].ValueImage = "value_5.gif";
-                    }
-
-
-                    if (items[i].RedStar == true)
-                    {
-                        items[i].RedStar2 = "red_star_72.png";
-                    }
-
-                    if (items[i].BlueBook == true)
-                    {
-                        items[i].BlueBook2 = "blue_book_72.png";
-                    }
-                }
-                catch (Exception e)
-                {
-                    DisplayAlert("警告", e.ToString(), "OK");
-                }
+                List1.Add(user.Name);
+            }
+            for (var j = 0; j < query2.Count; j++)
+            {                
+                items.Add(new Book { Name = List1[j], /*Value = 2.5*/ });
 
             }
+           
+            /*for (var j = 1; j == query2.Count; j++)
+            {                
+                foreach (var user in UserModel.countUser(j))
+                {
+                   items.Add(new Book { Name = user.Name, Value = 2.5 });
+
+                }
+            }*/
+
+            /*foreach (var user in query2)
+            {
+                List1[0] = user.Name;
+            }List1.Add = List1[0];*/
+
+
+
+            /*for (var i = 0; i < items.Count; i++)
+            {
+                if (items[i].Value <= 0.25)
+                {
+                    items[i].ValueImage = "value_0.gif";
+                }
+
+                else if (items[i].Value <= 0.75)
+                {
+                    items[i].ValueImage = "value_0.5.gif";
+                }
+
+                else if (items[i].Value <= 1.25)
+                {
+                    items[i].ValueImage = "value_1.gif";
+                }
+
+                else if (items[i].Value <= 1.75)
+                {
+                    items[i].ValueImage = "value_1.5.gif";
+                }
+
+                else if (items[i].Value <= 2.25)
+                {
+                    items[i].ValueImage = "value_2.gif";
+                }
+
+                else if (items[i].Value <= 2.75)
+                {
+                    items[i].ValueImage = "value_2.5.gif";
+                }
+
+                else if (items[i].Value <= 3.25)
+                {
+                    items[i].ValueImage = "value_3.gif";
+                }
+
+                else if (items[i].Value <= 3.75)
+                {
+                    items[i].ValueImage = "value_3.5.gif";
+                }
+
+                else if (items[i].Value <= 4.25)
+                {
+                    items[i].ValueImage = "value_4.gif";
+                }
+
+                else if (items[i].Value <= 4.75)
+                {
+                    items[i].ValueImage = "value_4.5.gif";
+                }
+
+                else
+                {
+                    items[i].ValueImage = "value_5.gif";
+                }
+
+
+                if (items[i].RedStar == true)
+                {
+                    items[i].RedStar2 = "red_star_72.png";
+                }
+
+                if (items[i].BlueBook == true)
+                {
+                    items[i].BlueBook2 = "blue_book_72.png";
+                }
+
+            }*/
 
             BookListView.ItemsSource = items;
 
@@ -108,8 +128,6 @@ namespace SamplePage
 
         public class Book
         {
-            public int ISBN { get; set; }
-
             public string Name { get; set; }
 
             public double Value { get; set; }
@@ -125,5 +143,12 @@ namespace SamplePage
             public string BlueBook2 { get; set; }
 
         }
+       /* private void OnItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            String x = new BookPage().BookListView.SelectedItem.ToString();
+            Navigation.PushAsync(new DetailPage(x));
+        }*/
+
+
     }
 }
