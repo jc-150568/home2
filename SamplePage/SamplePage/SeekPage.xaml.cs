296f0e6a063ed67a46b393c26d58fb9a6d279d73
遼太郎 金 <150568@jc-21.jp>
2018-01-22 12:07:20 +0900

    df

--- SamplePage/SamplePage/SeekPage.xaml.cs
+++ SamplePage/SamplePage/SeekPage.xaml.cs
@@ -16,5 +16,77 @@ namespace SamplePage
         {
             InitializeComponent();
         }
+        // 親カテゴリのプルダウンに応じて子カテゴリの内容を変更する
+        private void OnSelectedIndexChanged(object sender, EventArgs eventArgs)
+        {
+            try
+            {
+
+                if (this.picker2.SelectedIndex == 0)
+                {
+                    picker3.Items.Clear();
+                    picker3.Items.Add("少年");
+                    picker3.Items.Add("少女");
+                    picker3.Items.Add("青年");
+                    picker3.Items.Add("レディース");
+                }
+
+                if (this.picker2.SelectedIndex == 1)
+                {
+                    picker3.Items.Clear();
+                    picker3.Items.Add("語学学習");
+                    picker3.Items.Add("語学辞書");
+                    picker3.Items.Add("辞典");
+                    picker3.Items.Add("語学関係資格");
+                    picker3.Items.Add("学習参考書・問題書");
+                }
+
+                if (this.picker2.SelectedIndex == 2)
+                {
+                    picker3.Items.Clear();
+                    picker3.Items.Add("児童書");
+                    picker3.Items.Add("自動文庫");
+                    picker3.Items.Add("絵本");
+                    picker3.Items.Add("民話・昔話");
+                    picker3.Items.Add("しかけ絵本");
+                    picker3.Items.Add("図鑑・知識");
+                }
+
+
+                if (this.picker2.SelectedIndex == 3)
+                {
+                    picker3.Items.Clear();
+                    picker3.Items.Add("ミステリー・サスペンス");
+                    picker3.Items.Add("SF・ホラー");
+                    picker3.Items.Add("エッセイ");
+                    picker3.Items.Add("ノンフィクション");
+                    picker3.Items.Add("日本の小説");
+                    picker3.Items.Add("外国の小説");
+                }
+
+                if (this.picker2.SelectedIndex == 4)
+                {
+                    picker3.Items.Clear();
+                    picker3.Items.Add("ハードウェア");
+                    picker3.Items.Add("入門書");
+                    picker3.Items.Add("インターネット・WEBデザイン");
+                    picker3.Items.Add("ネットワーク");
+                    picker3.Items.Add("プログラミング");
+                    picker3.Items.Add("アプリケーション");
+                    picker3.Items.Add("OS");
+                    picker3.Items.Add("デザイン・グラフィックス");
+                    picker3.Items.Add("ITパスポート");
+                    picker3.Items.Add("MOUS・MOT");
+                    picker3.Items.Add("パソコン検定");
+                    picker3.Items.Add("IT・eコマース");
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                DisplayAlert("警告", e.ToString(), "OK");
+            }
+        }
     }
 }
